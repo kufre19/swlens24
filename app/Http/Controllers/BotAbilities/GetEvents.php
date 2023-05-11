acec6e9d47aa5182b8cd5c46b9c3b05b42ff034a
Emmanuel <whitemaxwell5@gmail.com>
2023-05-11 20:05:38 +0100

    update online

--- app/Http/Controllers/BotAbilities/GetEvents.php
+++ app/Http/Controllers/BotAbilities/GetEvents.php
@@ -5,9 +5,8 @@ namespace App\Http\Controllers\BotAbilities;
 use App\Http\Controllers\BotFunctions\GeneralFunctions;
 use App\Http\Controllers\BotFunctions\TextMenuSelection;
 use App\Models\ScheduleMenu;
-
-
-
+use App\Models\WaUser;
+use DateTime;
 
 class GetEvents extends GeneralFunctions implements AbilityInterface
 {
@@ -70,6 +69,19 @@ class GetEvents extends GeneralFunctions implements AbilityInterface
     {
     }
 
+    public function getUSerData()
+    {
+        $waUserModel = new WaUser();
+        $user = $waUserModel->where("whatsapp_id", $this->userphone)->first();
+
+        // check if required data are available 
+        if ($user->city == "") {
+            // go and ask user to register the info
+        }
+
+        return $user;
+    }
+
 
 
 
@@ -77,19 +89,26 @@ class GetEvents extends GeneralFunctions implements AbilityInterface
     {
         // first store any specific event needed by user
         $this->storeAnswerToSession(["store_as" => self::SPECIFIC_EVENT]);
+        $user = $this->getUSerData();
 
         // Set the URL endpoint
-        $url = "https://www.hebcal.com/hebcal?v=1&cfg=json&maj=on&min=on&il=es";
+        $url = "https://www.hebcal.com/hebcal?v=1&cfg=json&maj=on&min=on";
 
         // Add the current date to the URL as the default value for the "date" parameter
-        $url .= "&date=" . urlencode(date($this->user_session_data['answered_questions'][self::NEEDED_EVENT_DATE]));
+        $url .= "&start=" . urlencode(date("Y-m-d"));
+        $url .= "&end=" . urlencode(date($this->user_session_data['answered_questions'][self::NEEDED_EVENT_DATE]));
+
+        $url .= "&lg=" . urlencode($user->lang);
+
 
         // Add the city parameter to the URL
-        $url .= "&city=Madrid";
+        $url .= "&city=" . urlencode($user->city);
 
         // Loop through any additional parameters and add them to the URL
-        foreach ($params as $key => $value) {
-            $url .= "&" . $key . "=" . urlencode($value);
+        if (!empty($params)) {
+            foreach ($params as $key => $value) {
+                $url .= "&" . $key . "=" . urlencode($value);
+            }
         }
 
         // Initialize cURL session
@@ -103,65 +122,95 @@ class GetEvents extends GeneralFunctions implements AbilityInterface
         $response = curl_exec($ch);
 
         // Close the cURL session
-        curl_close($ch);
+
+        $arrayed_response = json_decode($response, true);
+      
+
+
+        $events = $arrayed_response['items'];
+        
+
+
+        // try to use regex to find a specific event else display the rest
+        if (!empty($events)) {
+            $matchingEvents = [];
+            if ($this->user_session_data['answered_questions'][self::SPECIFIC_EVENT] != "") {
+                $specified = $this->user_session_data['answered_questions'][self::SPECIFIC_EVENT];
+                info($specified);
+                foreach ($events as $item) {
+                    $title = $item['title'];
+                    if (preg_match("/\b$specified\b/i", $title)) {
+                        array_push($matchingEvents, $item);
+                    }
+                }
+            }
+        } else {
+            // tell user  that event not found for date given and end chat!
+        }
+
+
 
         // Return the response
-        return info($response) ;
+        curl_close($ch);
+        if(!empty($matchingEvents))
+        {
+            $this->showUserEvents($matchingEvents);
+        }else {
+            $text = "Sorry no events matched the one you specified but please see events for the date provided";
+            $message = $this->make_text_message($text, $this->userphone);
+            $this->send_post_curl($message);
+            $this->showUserEvents($events);
+
+        }
+
+        return $this->ResponsedWith200();
+    }
+
+
+    public function showUserEvents($events)
+    {
+        foreach ($events as $key => $event) {
+            $text = "";
+            $memo = "";
+            if (isset($event['memo'])) {
+                $memo = $event['memo'];
+            }
+            $pos = strpos($event['title'], ':');
+            if ($pos !== false) {
+                $title = substr($event['title'], 0, $pos);
+            }else{
+                $title = $event['title'];
+            }
+            $text .= "Event: {$title} {$memo}" . PHP_EOL;
+            $time = $event['date'];
+            $dateTime = new DateTime($time);
+            $formattedDate = $dateTime->format('l g:i A');
+            $text .= "Date: {$formattedDate}" . PHP_EOL;
+            $text .= "Hebrew: {$event['hebrew']}" . PHP_EOL;
+
+
+            $message = $this->make_text_message($text, $this->userphone);
+            $this->send_post_curl($message);
+        }
     }
 
 
 
     public function askForSpecificEvent()
     {
-        $message = "Any specific event in mind?";
+        $message = "Any specific event in mind ?";
         $message_obj = $this->make_text_message($message, $this->userphone);
         $this->send_post_curl($message_obj);
         $this->go_to_next_step();
         $this->ResponsedWith200();
     }
 
-    // public function test()
-    // {
-    //     return "test";
-    // }
 
-
-    public static function getGeonameId($city, $country)
-    {
-        // Replace YOUR_USERNAME with your Geonames API username
-        $username = "YOUR_USERNAME";
-
-        // Set up the API endpoint URL
-        $url = "http://api.geonames.org/searchJSON?q=" . urlencode($city) . "&country=" . urlencode($country) . "&maxRows=1&username=" . $username;
-
-        // Initialize a cURL session
-        $ch = curl_init();
-
-        // Set cURL options
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
-        // Execute the request
-        $response = curl_exec($ch);
-
-        // Close the cURL session
-        curl_close($ch);
-
-        // Parse the JSON response
-        $data = json_decode($response, true);
-
-        // Return the geoname ID (if found)
-        if (isset($data['geonames'][0]['geonameId'])) {
-            return $data['geonames'][0]['geonameId'];
-        } else {
-            return null;
-        }
-    }
 
 
     public function askForEventDate()
     {
-        $sample_date = date("d/m/Y");
+        $sample_date = date("Y-m-d");
         $message = "Please enter a date for event schedule you want to check! i.e {$sample_date}";
         $message_obj = $this->make_text_message($message, $this->userphone);
         $this->send_post_curl($message_obj);
@@ -170,8 +219,26 @@ class GetEvents extends GeneralFunctions implements AbilityInterface
     public function ValidateDate()
     {
         // validate and retrun false if date given is fasle 
-        $this->storeAnswerToSession(["store_as" => self::NEEDED_EVENT_DATE]);
-        return true;
+        $valid = true;
+
+        // Create a DateTime object from the date string
+        $date = DateTime::createFromFormat('Y-m-d', $this->user_message_original);
+
+        // Check if the date string matched the format 'YYYY-MM-DD'
+        if ($date && $date->format('Y-m-d') === $this->user_message_original) {
+            $valid = true;
+        } else {
+            // tell user to give proper date and end chat
+            $valid = false;
+        }
+
+
+        if ($valid) {
+
+            $this->storeAnswerToSession(["store_as" => self::NEEDED_EVENT_DATE]);
+            $this->go_to_next_step();
+            $this->continue_session_step();
+        }
     }
 
 
